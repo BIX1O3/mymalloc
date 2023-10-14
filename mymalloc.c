57eba814233ed23b99863113ae05048b8360467e
Bix103 <matthewbixby08@gmail.com>
2023-10-14 12:42:49 -0400

    updated mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -7,10 +7,10 @@
 static double memory[MEMLENGTH];
 
 
-void *malloc(size_t objsize){
+void *mallac(size_t objsize){
 
-    int objsize = (objsize+7) & ~7; //ensures that the object size is a multiple of 8 using bitwise &
-
+    int objsize_8byte = (objsize+7) & ~7; //ensures that the object size is a multiple of 8 using bitwise &
+    //printf("obj: %d\n", objsize_8byte);
     typedef struct MetaData{ //If we want to have two ints we can use two short ints which are only 2 bytes each
         int state;  //free = 0 and allocated is set to 1 
         int size;   //points to next metadata block 
@@ -18,64 +18,118 @@ void *malloc(size_t objsize){
 
     
     char *heap_start = (char*)memory;
-    MetaData* head = memory;
+    MetaData* head = (MetaData*) memory;
     void *ptr;
 
+    int node = 0;
+
     if ((*head).state == 0 && (*head).size == 0){
-        MetaData* newnode = (MetaData*)(heap_start+objsize);
         head->state = 1;
-        head->size = objsize+8;
+        head->size = objsize_8byte+8;
+
+        MetaData* newnode = (MetaData*)(heap_start+(head->size));
         newnode->state = 0;  //originally 0 so this is is redundant
         newnode->size = (MEMLENGTH * 8) - (head->size);
-
-        ptr = heap_start + 8;
+        //printf("1\n\n");
+        ptr = (void*)(heap_start + 8);
     } else {
-        MetaData* currentNode = head;
+        MetaData* currentNode = (MetaData*)heap_start;
         int position = 0;
-        int node = 0;
+        //int node = 0;
         int old_size = 0;
-        while(position < (MEMLENGTH*8) || node == 1) { //&& currentNode->state != 0 && currentNode->size != 0
-            
-            if (currentNode->state = 0 && currentNode->size >= objsize){
-                MetaData* newnode = (MetaData*)(currentNode+objsize); 
+        //printf("2.1\n");
+        while(position < (MEMLENGTH*8) && node == 0) { //&& currentNode->state != 0 && currentNode->size != 0
+
+            //printf("state: %d  size: %d", currentNode->state, currentNode->size);
+            //printf("\tposition: %d\n", position);
+
+            if (currentNode->state == 0 && currentNode->size >= objsize_8byte){
+                //printf("2.2\n\n");
+                
+                old_size = currentNode->size;
                 currentNode->state = 1;
-                currentNode->size = old_size;
-                currentNode->size = objsize+8;
+                currentNode->size = objsize_8byte+8;
+
+
+
+                MetaData* newnode = (MetaData*)((char*)currentNode+(currentNode->size));
+                printf("\n\nOldSize = %d\n",old_size);
+                //printf("%d %d\n\n", currentNode->state, currentNode->size);
+                
                 newnode->state = 0;  //originally 0 so this is is redundant
-                newnode->size = old_size - (objsize+8); // check to see if this logic works
+                newnode->size = old_size - (objsize_8byte+8); // check to see if this logic works
+
+                printf("\n\nNewSize = %d\n\n",newnode->size);
+
                 node = 1;
+                ptr = (void*)(currentNode+1);
+                //printf("2\n\n");
             }else{
-                currentNode = currentNode + currentNode->size;
+                //printf("3\n\n\n\n\n");
+                currentNode = (MetaData*)((char*)currentNode + currentNode->size);
             }
             position += currentNode->size;
-            
-            
-            /*currentNode = (MetaData*)(currentNode + currentNode->size);
-            position++;
-            if(position == 512) {
-                // idfk man
-            }*/
         }
-        /*MetaData* newnode = (MetaData*)(currentNode + objsize);
-        newnode->state = -1;
-        newnode->size = 4096 - (8*(position+objsize));
-        currentNode->state = 1;
-        currentNode-> size = objsize;*/
     }
-    
+    //printf("5\n\n");
+    //debugging loop checks the size of all of the nodes
+    MetaData* thisNode = (MetaData*)heap_start;
+    int count = 0;
+    while ((thisNode->size != 0 && thisNode->size !=0) && count < 4096){
+        //printf("runs\n");
+        printf("%d %d \n", thisNode->state, thisNode->size);
+        count += thisNode->size;
+        printf("count: %d\n\n", count);
+        if (count < 4096)    
+            thisNode = (MetaData*)(((char*)thisNode) + (thisNode->size));
+        
+    }
 
+    /*printf("first state: %d first size: %d\n", ((MetaData*)heap_start)->state, ((MetaData*)heap_start)->size);
+    printf("Second state: %d Second size: %d\n", (((MetaData*)heap_start+(((MetaData*)heap_start)->size)))->state, ((MetaData*)(heap_start+((MetaData*)heap_start)->size))->size);
 
+    if (node == 1){
+        MetaData* thisNode = (MetaData*)(heap_start+(((MetaData*)heap_start)->size));
+
+        MetaData* thisNode = (MetaData*)(((char*)thisNode) + (thisNode->size));
+        
+
+        printf("Third state: %d Third size: %d\n", here->state, here->size);
+    }*/
 
     return ptr;
 }
 
 
-void free(double *ptr){
+void free1(double *ptr){
 
 }
 
 int main(int argc, char** argv){
 
+    int* ptr = (int*)mallac(25);
+
+    int a = 5;
+
+    ptr = &a;
+
+    printf("a=%d  ptr = %p\n\n",*ptr, ptr);
+    printf("\n\n\n\n\n");
+    int* ptr2 = (int*)mallac(12);
+
+    int a2 = 5;
+
+    ptr2 = &a2;
+
+    printf("a2=%d  ptr2 = %p\n\n",*ptr2, ptr2);
+    printf("\n\n\n\n\n");
+    int* ptr3 = (int*)mallac(120);
+
+    int a3 = 5;
+
+    ptr3 = &a3;
+
+    printf("a3=%d  ptr3 = %p\n\n",*ptr3, ptr3);
 
     return 0;
 }
