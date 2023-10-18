998c6c6731e8a41476f9b8a8ce5451541544d22d
Bix103 <matthewbixby08@gmail.com>
2023-10-17 23:25:13 -0400

    corrected header and added memCleared declaration

--- mymalloc.h
+++ mymalloc.h
@@ -1,14 +1,12 @@
 #ifndef MYMALLOC_H
 #define MYMALLOC_H
 
+void *mymalloc(size_t size, char* file, int line);
+void myfree(void *ptr, char *file, int line);
+int memcleared();
 
 #define malloc(x) mymalloc(x,__FILE__, __LINE__)
 #define free(x) myfree(x, __FILE__, __LINE__)
-
-
-void *mymalloc(size_t size, char* file, int line);
-void myfree(void *ptr, char *file, int line);
-
 
 
 
