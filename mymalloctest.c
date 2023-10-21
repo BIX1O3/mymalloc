55f27505dbc96d65d8cb18bbd49cca667680bf1f
BIX1O3 <124328118+BIX1O3@users.noreply.github.com>
2023-10-20 20:51:02 -0400

    Added comments

--- mymalloctest.c
+++ mymalloctest.c
@@ -4,15 +4,25 @@
 #include "mymalloc.h"
 #endif
 
-//Function creates errors that malloc and free need to be able to detect
+
+
+/*******************************************
+ * TEST Mandatory MYMALLOC Error Reporting
+ * Calls free on an address not obtained by malloc
+ * Calls free on an address that is not the start of the chunk
+ * Calls free on an address for the second time
+ *******************************************/
 void detecting_errors(){
+    printf("Function tests mymalloc's error reporting\n\nTest 1: Calls free on an address not obtained by malloc\nExpected Output: \"Error: Unable to free memory address\"\n");
 
     int x;
     free(&x);
 
+    printf("\nTest 2: Calls free on an address that is not the start of the chunk\nExpected Output: \"Error: Unable to free memory address\"\n");
     int *p = malloc(sizeof(int)*2);
     free(p+1);
 
+    printf("\nTest 3: Calls free on an address for the second time\nExpected Output: \"Error: Unable to free memory address\"\n");
     int *t = malloc(sizeof(int)*100);
     int *q = t;
     free(t);
@@ -20,24 +30,29 @@ void detecting_errors(){
 
 
 
-    //clean memory for other test cases
-    free(p);
+    
+    free(p); //clean memory for other test cases
 }
 
 
-//Fills array then frees all pointers two times
+/*******************************************
+ * TEST Memory capicity
+ * Calls 256 1 byte malloc calls to fill memory
+ * Calls on the 256 pointer to ensure proper coalescing 
+ * Repeats malloc and free calls to test on an initialized memory
+ *******************************************/
 void malloc_test1(){
+    printf("\nFunction fills memory with 256 1 byte malloc calls then frees 256 times, then repeats\nExpected Output: \"MemClear?: 1\"\n");
+
     int count = 0;
     char *ptrArray[256];
 
     while (count < 256){
         ptrArray[count] = malloc(1);
-        //printf("%d\n",count);
         count++;
     }
     count = 0;
     while (count < 256){
-        //printf("%d\n",count+1);
         free(ptrArray[count]);
         
         count++;
@@ -47,43 +62,47 @@ void malloc_test1(){
     count = 0;
     while (count < 256){
         ptrArray2[count] = malloc(1);
-        //printf("%d\n",count);
         count++;
     }
     count = 0;
     while (count < 256){
-        //printf("%d\n",count+1);
         free(ptrArray2[count]);
-        
         count++;
     }
 
-    printf("\nMemClear?: %d\n\n", memCleared());
-
+    printf("MemClear?: %d\n", memCleared());
 }
 
 
-//Tests for when client calls malloc with to many bytes
-//Tests for when client calls malloc with 0 bytes
+/*******************************************
+ * TEST Unexptable client malloc inputs
+ * Calls malloc with 9999999 bytes to test for malloc client requests larger than memory
+ * Calls malloc with 0 bytes to test for malloc client requests for no bytes 
+ *******************************************/
 void malloc_test2(){
+    printf("\nFunction tests client request for 0 bytes and more bytes than memory\nExpected Output: \"Error: Cannot allocate 9999999 byte(s)\"\nExpected Output: \"Error: Cannot allocate 0 bytes\"\n");  
     void *ptr = malloc(9999999);
     void *ptr2 = malloc(0);
 }
 
 
-//Tests for when client calls malloc with in range byte amount, but there isn't enough memory to allocate 
+/*******************************************
+ * TEST malloc call for bytes that memory is unable to allocate
+ * Calls malloc 4000 bytes and then with 100 bytes to purposefully go over the memory size
+ * Calls free on a NULL pointer recieved from malloc
+ *******************************************/
 void malloc_test3(){
+    printf("\nFunction tests client request for more bytes than the memory can allocate for\nExpected Output: \"Error: Not enough memory to allocate 100 byte(s)\"\nExpected Output: \"Error: Cannot Deallocate NULL Pointer\"\n");  
     void *ptr =malloc(4000);
-    void *ptr2 = malloc(100);
+    void *ptr2 = malloc(100); //purposefully calls malloc with too many bytes
 
-
-    //clean memory
-    free(ptr);
+    free(ptr2); //purposefully free a NULL ptr returned by malloc
     
+
+
+
+    free(ptr); //clean memory
 }
-
-
-
 
 
 int main(int argc, char** argv){
@@ -91,7 +110,5 @@ int main(int argc, char** argv){
     malloc_test1();
     malloc_test2();
     malloc_test3();
-    
-
     return 0;
 }
